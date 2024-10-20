4e699f5819e65b7df929dfbb3bed83abbaccb15f
Anis <anis2k20@gmail.com>
2024-10-20 23:29:51 +0600

    hero section design ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'custom': '0 2px 28px 0 rgba(0, 0, 0, 0.09)',
+        'light-shadow': '0 2px 28px 0 rgba(0, 0, 0, 0.09)',
+        'dark-shadow': '0 2px 40px 0 rgba(255, 255, 255, 0.05)'
       },
       colors: {
         lightBackground: '#f9f9f9',
         lightForeground: '#ffffff',
+        darkBackground: '#1E1E1E',
+        darkForeground: '#0F0F0F',
         dark:'#212121',
         primary: '#2857FC'
       },
