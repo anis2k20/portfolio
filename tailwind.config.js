9816ca8e1080350c9c3bbdf582631e23789bd4b1
Anis <anis2k20@gmail.com>
2024-10-20 14:27:15 +0600

    hero section header and programming icon added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {
+      boxShadow: {
+        'custom': '0 2px 28px 0 rgba(0, 0, 0, 0.09)',
+      },
+      colors: {
+        lightBackground: '#f9f9f9',
+        lightForeground: '#ffffff',
+        dark:'#212121',
+        primary: '#2857FC'
+      },
       fontFamily: {
         'mulish': ['Mulish', 'sans-serif'],
         'poppins': ['Poppins', 'sans-serif'],
-      }
+      },
+
     },
   },
   plugins: [],
